f4fa9310c0701865a391c2772f2d44ef36aad569
Christopher Mattar <69043333+3DCoded@users.noreply.github.com>
2024-08-23 15:26:08 -0400

    test(mmms): Reinit printer to get updated variables

--- panels/mmms.py
+++ panels/mmms.py
@@ -220,6 +220,7 @@ class Panel(ScreenPanel):
     
     def reload(self, widget):
         self._screen.show_popup_message('Reloading 3MS Control', 1)
+        self._printer.reinit()
         self.buttons['reload'].set_label('Reloaded')
         self.mmms_settings = self._printer.data['configfile']['config']['gcode_macro MMMS_SETTINGS']
         logging.info(self.mmms_settings)
