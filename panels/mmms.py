9c84bb5a1265fd8b10805717f4f8b66b504dd645
Christopher Mattar <69043333+3DCoded@users.noreply.github.com>
2024-08-23 14:52:20 -0400

    test: Log dir() of reload button

--- panels/mmms.py
+++ panels/mmms.py
@@ -220,6 +220,7 @@ class Panel(ScreenPanel):
     
     def reload(self, widget):
         self._screen.show_popup_message('Reloading 3MS Control', 1)
+        logging.info(dir(self.buttons['reload']))
         self.change_selected_tool(None, 0)
     
     def sync_tool(self, widget):
